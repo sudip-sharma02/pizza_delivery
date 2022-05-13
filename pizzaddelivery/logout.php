6dbd716f914e0f473d89ede5d0f9af38852f4e04
sudip022 <suipsharma20287@gmail.com>
2022-05-13 16:45:02 +0545

    uploading the project in git

---
+++ pizzaddelivery/logout.php
@@ -0,0 +1,236 @@
+<?php
+// clear all the session variables and redirect to index
+session_start();
+session_unset();
+session_write_close();
+$url = "./index.php";
+header("Location: $url");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crust Online Pizza</title>
+    <link rel="stylesheet" href="style.css">
+	 <meta charset="UTF-8">
+	 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="style1.css">
+	
+</head>
+<body>
+
+    <!-------------------header section start-->
+    <section class="header">
+        <nav>
+            <a href=""><img src="" class="logo"></a>
+            <nav id="navbar">
+            <div class="nav-link">
+                <ul id="menu">
+                    <li class="link"><a href="index.html">HOME</a></li>
+                    <li class="link"><a href="">ABOUT US</a></li>
+                    <li class="link"><a href="">SERVICES</a></li>
+					<li class="link"><a href="login.php"> LOGIN  </a></li>
+                  
+                </ul>
+            </div>
+            
+    </nav>
+        </nav>
+        <div class="text-box">
+            <h1>Crust Online Pizza </h1>
+            <p> Best Pizza, Best Value </p>
+            <BR>
+                <a href=""class="hero-btn">ORDER NOW</a>
+                
+        </div>
+
+    </section>
+  <!-------------------header section end-->
+
+    <!----main body  start----->
+
+
+    <section class="services">
+        <h1>Our Services</h1>
+            <div class="row">
+            <div class="services-col">
+            <img src="images/pizza1.jpg" style="height:200px; width:292px;">
+			
+                <h3>Home Delivery </h3>
+                <p> </p>
+            </div>
+            <div class="services-col">
+                <img src="images/pizza4.jpg" style="height:200px; width:230px;">
+                    <h3> store pickup  </h3>
+                    <p> </p>
+                </div>
+                <div class="services-col">
+            <img src="images/pizza5.jpg" style="height:200px; width:285px;">
+                <h3>  Cash Payment  </h3>
+                <p>  </p>
+            </div>
+            </div>
+
+        </section>
+
+        <section class="services">
+            <h1>TODAYS SPECIAL</h1>
+                <div class="row">
+                <div class="services-col">
+                <img src="images/pizza9.jpg" style="height:250px; width:250px;">
+                    <h3>BBQ chicken</h3>
+
+			<br>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+                    <p> </p>
+                </div>
+                <div class="services-col">
+                    <img src="images/pizza2.webp" style="height:245px; width:300px;">
+                        <h3> Pepperoni </h3>
+						<BR>
+                <a href=""  class="hero-btn"> ORDER NOW </a>
+                        <p> </p>
+                    </div>
+                    <div class="services-col">
+                <img src="images/pizza10.jpg" style="height:220px;">
+                    <h3> Italian pizza platter </h3>
+					<BR>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+					
+                    <p>  </p>
+                </div>
+                </div>
+    
+            </section>
+
+
+            <section class="services">
+                <h1><B> CRUST OFFER OF THE  WEEK  </h1>
+                    <div class="row">
+                    <div class="services-col">
+                    <img src="images/pizza6.jpg">
+                        <h3> 30% OFF ON HAWAIIAN LARGE  </h3>
+						<BR>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+                        <p>  </p>
+                    </div>
+                    <div class="services-col">
+                        <img src="images/pizza11.jpg" style="height:230px; width:350px;">
+                            <h3> MARGHERITA   [half price] </h3>
+							<BR>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+                            <p> </p>
+                        </div>
+                        <div class="services-col">
+                    <img src="images/pizza7.jpg">
+                        <h3> SALUMI       [free delivery]  </h3>
+						<BR>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+                        <p>   </p>
+                    </div>
+                    </div>
+					 <section class="services">
+            <h1> CRUST REGULAR MENU </h1>
+                <div class="row">
+                <div class="services-col">
+                <img src="images/pizza12.jpg" style="height:210px; width:292px;">
+                    <h3> VEGGIE PIZZA </h3>
+					<BR>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+			
+                    <p> </p>
+                </div>
+                <div class="services-col">
+                    <img src="images/pizza16.jpg" style="height:260px; width:290px;">
+                        <h3> chilli cheese pizza   </h3>
+						<BR>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+                        <p> </p>
+                    </div>
+                  
+				 <div class="services-col">
+                    <img src="images/pizza13.jpg" style="height:220px; width:300px;">
+                        <h3>crust chilli pizza </h3>
+						<BR>
+                <a href=""class="hero-btn"> ORDER NOW </a>
+                        <p> </p>
+                    </div>
+					
+				
+              
+				
+
+            </section>
+		</section>
+        
+    <footer class="footer-distributed">
+
+        <div class="footer-left">
+            <h3> Crust <span> online 
+Pizza </span></h3>
+
+            <p class="footer-links">
+                <a href="#">Home</a>
+                |
+                <a href="#">About</a>
+                |
+                <a href="#">Contact</a>
+                |
+                <a href="#">Blog</a>
+            </p>
+
+            <p class="footer-company-name">Copyright © 2021 <strong> crust online pizza </strong> All rights reserved</p>
+        </div>
+
+        <div class="footer-center">
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span></span>
+                    Huntingdale </p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <a href="tel:0410933007"> 0410933007 </a>
+            </div>
+            <div>
+                <i class="fa fa-envelope"></i>
+            <p><a href="mailto: crust online @gmail.com ">crust56@gmail.com </a></p>
+            </div>
+        </div>
+        <div class="footer-right">
+            <p class="footer-company-about">
+                <span>About the company</span>
+                <strong><b> crust online pizza </strong>
+
+				is a reputed pizza shop in the eastern suburbs 
+				of huntingdale vic. It has been operating it's business since 2016 delivering the 
+				varities of pizza online to our valuable customer. we offer hand strectch pizza with 
+				wooden fire flavour.</b>
+            </p>
+            <div class="footer-icons">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a> 
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-youtube"></i></a>
+            </div>
+
+        </div>
+    </footer>
+        
+                </section>
+
+
+
+         
+
+
+
+</body>
+</html>
+
